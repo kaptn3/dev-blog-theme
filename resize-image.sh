ad204361719c443fa79bae38025998833b2e982b
victoria <kapitonenko.vick@ya.ru>
2019-07-27 18:00:41 +0300

    feat: update script

--- resize-image.sh
+++ resize-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-convert -resize 695X img/$1 img/$1
+mogrify -path img/ -filter Triangle -define filter:support=2 -thumbnail 800 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 95 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB img/$1
